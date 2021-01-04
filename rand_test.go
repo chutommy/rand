d42e2648f4937ddd65ec2622f0efd3b898261cd7
chutified <tommychu2256@gmail.com>
2021-01-04 11:32:24 +0100

    Add tests and benchmarks for rand functions

--- rand_test.go
+++ rand_test.go
@@ -1,25 +1,25 @@
-package rand
+package rand_test
 
 import (
-	rand "math/rand"
-	"reflect"
 	"testing"
+
+	"github.com/chutified/rand"
 )
 
 func TestRand(t *testing.T) {
+	r1 := rand.New()
+	r2 := rand.New()
 
-	r := New()
+	got1 := r1.Uint64()
+	got2 := r2.Uint64()
 
-	got := reflect.TypeOf(r).String()
-	exp := reflect.TypeOf(&rand.Rand{}).String()
-	if got != exp {
-		t.Errorf("Function New returns %s, expected %s", got, exp)
+	if got1 == got2 {
+		t.Errorf("highly unlikely to generate same integers: %d", got1)
 	}
 }
 
 func BenchmarkRand(b *testing.B) {
-
-	r := New()
+	r := rand.New()
 
 	for n := 0; n < b.N; n++ {
 		r.Float64()
--- source_test.go
+++ source_test.go
@@ -1,25 +1,26 @@
-package rand
+package rand_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/chutified/rand"
+)
 
 func TestSource(t *testing.T) {
-
-	src := newSource()
+	src := rand.NewSource()
 
 	src.Seed(0) // no-op, for coverage purpose
 
 	// validate int63
 	i := src.Int63()
 	if int64(uint64(i)&uint64(1<<63)) != 0 {
-		t.Error("Value returned by Int63() is higher than 63-bit integer.")
+		t.Error("Value returned by Int63() does not fit into the 63-bit integer.")
 	}
 }
 
 func BenchmarkSource(b *testing.B) {
-
-	src := newSource()
-
 	for n := 0; n < b.N; n++ {
+		src := rand.NewSource()
 		src.Int63()
 	}
 }
